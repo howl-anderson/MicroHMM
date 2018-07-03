a830c80847fc17c17efcc466caf74e91d42260d7
Xiaoquan Kong <u1mail2me@gmail.com>
2018-07-03 12:31:01 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+requirements = ['networkx==2.1']
+test_requirements = ['pytest==3.5.1']
+
 setup(
     name='MicroHMM',
     version='0.1',
     packages=['MicroHMM'],
     url='',
+    install_requires=requirements,
+    tests_require=test_requirements,
     license='MIT',
     author='Xiaoquan Kong',
     author_email='u1mail2me@gmail.com',
