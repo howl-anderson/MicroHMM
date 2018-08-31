a13384adb78945bb3e6eca7d228e3a10c1bd5f77
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-31 22:44:38 +0800

    Update test case

--- tests/test_save_model.py
+++ tests/test_save_model.py
@@ -1,28 +1,19 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
-import pathlib
-from unittest import mock
 
 from MicroHMM.hmm import HMMModel
 
 
-def test_save_model():
-    model_dir = "some_dir"
+def test_save_model(tempdir):
+    model_dir = tempdir.mkdir("some_dir")
+    model_dir_str = str(model_dir)
 
     A = {}
     B = {}
 
     vocabulary = set()
 
-    with mock.patch.object(HMMModel, '_save_data',
-                           return_value=None) as mock_method:
-        hmm_model = HMMModel(A, B, vocabulary)
-        hmm_model.save_model(model_dir)
+    hmm_model = HMMModel(A, B, vocabulary)
+    hmm_model.save_model(model_dir_str)
 
-    model_dir_path = pathlib.Path(model_dir)
-
-    mock_method.assert_has_calls([
-        mock.call(A, model_dir_path / 'A.json'),
-        mock.call(B, model_dir_path / 'B.json'),
-        mock.call(vocabulary, model_dir_path / 'vocabulary.json')
-    ], any_order=False)
+    assert len(model_dir.listdir()) == 3
