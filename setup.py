4b986d04209f8535e72d8df970bee8f168f20174
Xiaoquan Kong <u1mail2me@gmail.com>
2018-09-02 12:53:13 +0800

    Version updated from 0.1.2 to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ test_requirements = ['pytest==3.5.1']
 
 setup(
     name='MicroHMM',
-    version='0.1.2',
+    version='0.2.0',
     packages=['MicroHMM'],
     url='https://github.com/howl-anderson/MicroHMM',
     install_requires=requirements,
